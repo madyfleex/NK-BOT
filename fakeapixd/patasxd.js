a817a9660b8f0487a5fb10c714eac6427dfe3f96
madyfleexGT <89738692+madyfleex@users.noreply.github.com>
2021-09-01 10:02:01 -0400

    Update patasxd.js

--- fakeapixd/patasxd.js
+++ fakeapixd/patasxd.js
@@ -1,5 +1,6 @@
 [
 {"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZEY4qQmwAJhKVFlypt0nZNgzrkApOax6vew&usqp=CAU"},
 {"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfyqYAyrxqYswPhrZIfJz_wvS_56-zpPoWqg&usqp=CAU"}, 
-{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjFEru2gdPL3xI5FahQsRw8GPLbWTEjaUgWQ&usqp=CAU"}
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjFEru2gdPL3xI5FahQsRw8GPLbWTEjaUgWQ&usqp=CAU"}, 
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSX7isHMaKks5o36xHrjbaLp69dgPKrUpw1Nw&usqp=CAU"}
 ]
